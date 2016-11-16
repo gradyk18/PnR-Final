40ce3cf1bc26b20325c5336c41d27bd65d48809d
gradyk <gradyk@gilmour.org>
2016-11-16 09:01:32 -0500

    updated my pigo

--- pigo.py
+++ pigo.py
@@ -46,9 +46,18 @@ class Pigo(object):
         menu.get(ans, [None, error])[1]()
 
     def openHouse(self):
-        while True:
-            if not self.isClear():
-                self.beShy()
+        choice = input("1) Shy;  2) Spin.. ")
+        if choice == "1":
+            while True:
+                if not self.isClear():
+                    self.beShy()
+        else:
+            while True:
+                if not self.isClear():
+                    for x in range(5):
+                        self.encR(2)
+                        self.encL(2)
+                    self.encR(15)
 
     def beShy(self):
         set_speed(80)
@@ -129,20 +138,19 @@ class Pigo(object):
         time.sleep((enc / 18) * 1.8)
         stop()
 
-    # HELP STUDENTS LEARN HOW TO PORTION ENCODE VALUES
+    # HELP STUDENTS LEARN HOW TO PORTION TURN/SLEEP VALUES
     def rotate(self):
-        # initial encoder value = 1 wheel rotation
-        enc = 18
+        print("We tell our robot to rotate then pause the app.")
+        print("The longer the pause, the longer the turn.")
+        print("We also like to slow our robot down for the turn.")
         while True:
-            select = input('Right, left or encode? (r/l/e): ')
-            if select == 'r':
-                self.encR(enc)
-            elif select == 'l':
-                self.encL(enc)
-            elif select == 'e':
-                enc = int(input('New encode value: '))
-            else:
-                break
+            speed_adj = float(input("What modifier would you like to apply to your speed?"))
+            set_left_speed(int(self.LEFT_SPEED * speed_adj))
+            set_right_speed(int(self.RIGHT_SPEED * speed_adj))
+            turn_time = float(input("How many seconds would you like to turn? "))
+            right_rot()
+            time.sleep(turn_time)
+            self.stop()
 
     ##DUMP ALL VALUES IN THE SCAN ARRAY
     def flushScan(self):
@@ -250,7 +258,7 @@ class Pigo(object):
                 set_left_speed(self.LEFT_SPEED)
                 set_right_speed(self.RIGHT_SPEED)
                 print("Left: " + str(self.LEFT_SPEED) + "//  Right: " + str(self.RIGHT_SPEED))
-                self.encF(25)
+                self.encF(9)
                 response = input("Reduce left, reduce right or done? (l/r/d): ")
                 if response == 'l':
                     self.LEFT_SPEED -= 10
